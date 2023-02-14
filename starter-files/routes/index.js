d23f432404a1aa860eabab3b9ce2beb15a0a449d
Hosam Hamdy <hosam5553@gmail.com>
2023-02-14 20:33:23 +0200

    at vid 10

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const storeController = require('../controllers/storeController');
 // Do work here
-router.get('/', (req, res) => {
-  // res.send('Hey! It works!');
-  // const hos = { name: 'Hosam', Age: '31' };
-  // res.json(hos);
-  res.render('hello', { name: req.query.name });
-});
-
-// router.get('/reverse/:name', (req, res) => {
-//   const reverse = [...req.params.name].reverse().join('');
-//   res.send(reverse);
-// });
+router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 module.exports = router;
