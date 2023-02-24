bcc68f4e6d82cf661fd823b75b00460fd92e02be
Hosam <hosam5553@gmail.com>
2023-02-24 02:51:08 +0200

    at Module 7

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter: function (req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: "The supplied filetype isn't allowed!" }, false);
+    }
+  },
+};
 
 exports.homePage = (req, res) => {
   res.render('index');
@@ -7,6 +22,22 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
+};
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  if (!req.file) {
+    next();
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
 };
 
 exports.createStore = async (req, res) => {
@@ -32,6 +63,8 @@ exports.editStore = async (req, res) => {
 };
 
 exports.updateStore = async (req, res) => {
+  // set the location data to be a Point (as intended in the schema) because updates on MongoDB by default omits defaults
+  req.body.location.type = 'Point';
   // find and update the store
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true, //returns the new store instead of the old one
@@ -40,7 +73,14 @@ exports.updateStore = async (req, res) => {
   // redirect them to the store and tell them it worked
   req.flash(
     'success',
-    `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store 👀</a>`
+    `Successfully updated <strong>${store.name}</strong>. <a href="/store/${store.slug}">View Store 👀</a>`
   );
   res.redirect(`/stores/${store._id}/edit`);
+};
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  console.log(store);
+  if (!store) return next();
+  res.render('store', { store, title: store.name });
 };
