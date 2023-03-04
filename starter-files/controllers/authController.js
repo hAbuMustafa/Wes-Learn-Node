3078fc291cb8b6ec712b4d631735a5fe59025ed3
Hosam Hamdy <hosam5553@gmail.com>
2023-03-04 20:31:28 +0200

    at vid 26

---
+++ starter-files/controllers/authController.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+
+exports.login = passport.authenticate('local', {
+  failureRedirect: '/login',
+  failureFlash: 'Failed Login!',
+  successRedirect: '/',
+  successFlash: 'You are now logged in!',
+});
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash('success', 'You are now logged out! 👋');
+  res.redirect('/');
+};
+
+exports.isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+    return;
+  }
+  req.flash('error', 'Oops! You gotta be logged in to do that!');
+  res.redirect('/login');
+};
