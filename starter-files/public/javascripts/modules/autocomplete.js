27a677d59854c73616e73bad0e1734e80b4b5f2d
Hosam Hamdy <hosam5553@gmail.com>
2023-03-17 13:26:42 +0200

    at vid 33

--- starter-files/public/javascripts/modules/autocomplete.js
+++ starter-files/public/javascripts/modules/autocomplete.js
@@ -1,15 +1,17 @@
 export default function autocomplete(input, latInput, lngInput) {
-  const dropdown = new google.maps.places.Autocomplete(input, {
-    fields: ['formatted_address', 'geometry', 'name'],
-  });
+  const dropdown = input
+    ? new google.maps.places.Autocomplete(input, {
+        fields: ['formatted_address', 'geometry', 'name'],
+      })
+    : null;
 
-  dropdown.addListener('place_changed', () => {
+  dropdown?.addListener('place_changed', () => {
     const place = dropdown.getPlace();
     latInput.value = place.geometry.location.lat();
     lngInput.value = place.geometry.location.lng();
   });
 
-  input.on('keydown', (e) => {
+  input?.on('keydown', (e) => {
     if (e.keyCode === 13) e.preventDefault();
   });
 }
