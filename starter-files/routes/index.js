1f1a51b14e39613e39a3ba8c48fc3818cf3b76de
Hosam Hamdy <hosam5553@gmail.com>
2023-03-26 13:55:18 +0200

    at vid 38

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 const { catchErrors } = require('../handlers/errorHandlers');
 // Do work here
 router.get('/', catchErrors(storeController.getStores));
@@ -54,6 +55,12 @@ router.post(
 );
 
 router.get('/map', storeController.mapPage);
+router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
+router.post(
+  '/reviews/:id',
+  authController.isLoggedIn,
+  catchErrors(reviewController.addReview)
+);
 
 /* 
 API Routes
@@ -61,6 +68,5 @@ API Routes
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
 router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
-router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
 
 module.exports = router;
--- starter-files/start.js
+++ starter-files/start.js
@@ -22,6 +22,7 @@ mongoose.connection.on('error', (err) => {
 // READY?! Let's go!
 require('./models/Store');
 require('./models/User');
+require('./models/Reviews');
 
 // Start our app!
 const app = require('./app');
